2e86f4342a09d30ea37deefd97db34114ab54dfb
debtman <ip-soft@list.ru>
2019-09-17 12:13:36 +0600

    Add project files.

---
+++ NetCoreDI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreDI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
